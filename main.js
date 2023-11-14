1722ecb8ae53065deae28baec821d5fecd43ccb3
Thang Vu <thangvu.dona@gmail.com>
2023-11-14 08:00:07 +0700

    11 - Custom output for API LIST
    - get totalCount from responseHeader['x-total-count']
    _ get '_page' and '_limit' from req._parsedUrl.query
    - custom return result includes data and pagination info

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const jsonServer = require('json-server')
+import queryString from 'query-string'
+import jsonServer from 'json-server'
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -24,6 +25,37 @@ server.use((req, res, next) => {
   // Continue to JSON Server router
   next()
 })
+
+// In this example, returned resources will be wrapped in a body property
+// Custom output for LIST with pagination
+router.render = (req, res) => {
+  // Check GET with pagination
+  // If yes, custom output
+  const headers = res.getHeaders()
+
+  const totalCountHeader = headers['x-total-count']
+  if (req.method === 'GET' && totalCountHeader) {
+    const { _limit, _page } = queryString.parse(req._parsedUrl.query);
+
+    console.log(res['x-total-count']);
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(_page) || 1,
+        _limit: Number.parseInt(_limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader)
+      }
+    }
+
+    return res.jsonp(result);
+  }
+  // Otherwise, keep default behavior
+
+  res.jsonp({
+    body: res.locals.data
+  })
+}
 
 // Use default router
 server.use('/api', router)
